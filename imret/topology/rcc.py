fix rcc8 computation

imret/topology/rcc.py
@@ -13,11 +13,14 @@ class Relation(object):
                  'TPP': 'Tangential proper part',
                  'TPPi': 'Tangential proper part inverse',
                  'NTPP': 'Non-Tangential proper part',
-                 'NTPPi': 'Non-Tangential proper part inverse'}
+                 'NTPPi': 'Non-Tangential proper part inverse',
+                 'UNK': 'Unknown'}
 
     def __init__(self, scope):
         self.scope = scope
-        self.relation = collections.OrderedDict({'rcc8': scope, 'rcc5': scope})
+        self.relation = collections.OrderedDict()
+        self.relation['rcc8'] = scope
+        self.relation['rcc5'] = scope
 
     def __repr__(self):
         return "RCC relation {}: {}".format(self.scope, self._longname[self.scope])
@@ -34,6 +37,17 @@ class Relation(object):
 
 
 class RCC(object):
+    rx = 64
+    ry = 128
+    ra = 192
+
+    @property
+    def x_value(self):
+        return self.rx
+
+    @property
+    def y_value(self):
+        return self.ry
 
     def compute(self, shape, object1, object2, kernel=None):
 
@@ -46,9 +60,10 @@ class RCC(object):
         return self.detect(imx, imy, kernel)
 
     def detect(self, imx, imy, kernel=None):
-
         hx, hy, ha = self._intersection(imx, imy)
+        return self.decision(imx, imy, hx, hy, ha, kernel=kernel)
 
+    def decision(self, imx, imy, hx, hy, ha, kernel=None):
         # define relations
         if hx == 0 and hy == 0 and ha != 0:
             return Relation(scope='EQ')
@@ -60,30 +75,36 @@ class RCC(object):
         kernel = kernel or np.ones((11, 11), dtype=np.uint8)
 
         if hx == 0 and hy != 0 and ha != 0:
+            # PP, but which one?
             imd = cv2.dilate(imx, kernel)
             dilation, _, _ = self._intersection(imd, imy)
-            if dilation:
-                return Relation(scope='NTPP')
+            if dilation != 0:
+                return Relation(scope='TPP')
             else:
+                if not self.is_border(imx):
+                    return Relation(scope='NTPP')
                 return Relation(scope='TPP')
 
         if hx != 0 and hy == 0 and ha != 0:
+            # PPi, but which one?
             imd = cv2.dilate(imy, kernel)
-            dilation, _, _ = self._intersection(imx, imd)
-            if dilation:
-                return Relation(scope='NTPPi')
+            _, dilation, _ = self._intersection(imx, imd)
+            if dilation != 0:
+                return Relation(scope='TPPi')
             else:
+                if not self.is_border(imy):
+                    return Relation(scope='NTPPi')
                 return Relation(scope='TPPi')
 
         if hx != 0 and hy != 0 and ha == 0:
             imd = cv2.dilate(imx, kernel)
             _, _, dilation = self._intersection(imd, imy)
-            if dilation:
-                return Relation(scope='DC')
+            if dilation != 0:
+                return Relation(scope='EC')
             else:
-                Relation(scope='EC')
+                Relation(scope='DC')
 
-        return Relation(scope='DC')
+        return Relation(scope='UNK')
 
     def _intersection(self, ix, iy):
         """
@@ -92,15 +113,16 @@ class RCC(object):
         :param iy: image y
         :return: area of regions (x, y, intersection)
         """
-        im = ix + iy  # cv2.bitwise_or(ix, iy)
-        # freq = cytoolz.frequencies(im.ravel())
-        # hx = freq.get(1, 0)  # x alone
-        # hy = freq.get(2, 0)  # y alone
-        # ha = freq.get(3, 0)  # x & y
-
+        im = ix + iy
         freq = np.bincount(im.ravel().astype(np.int64))
-        hx = freq[1] if len(freq) > 1 else 0
-        hy = freq[2] if len(freq) > 2 else 0
-        ha = freq[3] if len(freq) > 3 else 0
+        hx = freq[self.rx] if len(freq) > self.rx else 0
+        hy = freq[self.ry] if len(freq) > self.ry else 0
+        ha = freq[self.ra] if len(freq) > self.ra else 0
 
         return hx, hy, ha
+
+    def is_border(self, img):
+        return img[:, 0].sum() > 0 or \
+               img[:, -1].sum() > 0 or \
+               img[0, :].sum() > 0 or \
+               img[-1, :].sum() > 0
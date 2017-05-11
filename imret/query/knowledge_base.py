a28568c341158b9f464ff0ee1006de3b3d73be89
Danilo Nunes <nunesdanilo@gmail.com>
2017-05-11 01:41:14 -0300

    prover fixes

--- imret/query/knowledge_base.py
+++ imret/query/knowledge_base.py
@@ -46,9 +46,8 @@ class KnowledgeBase:
             axiom_ = "(holdsDuring Now (orientation {} {} {}))".format(obj1, obj2, prep)
             yield axiom_.replace("_", "")
 
-    def tptp_by_image(self, image):
+    def tptp_by_image(self, image, imindex=0):
         imgname = image
-        imindex = self.images.index(image)
         frame = self.df[self.df['image'] == imgname]
 
         objects = set(frame.object1) | set(frame.object2)
@@ -66,16 +65,19 @@ class KnowledgeBase:
             yield "fof(kb_IRRC_{},axiom,(( s__orientation(s__{}__m,s__{}__m,s__{})))).".format(self.index, obj1, obj2, prep)
             self.index += 1
 
-    def tptp_query(self, obj1, obj2, preposition):
+    def tptp_query(self, obj1, obj2, preposition, index=0):
+        obj1_ = "{}{}".format(obj1, index)
+        obj2_ = "{}{}".format(obj2, index)
         # prep = preposition.title().replace("_", "")
         # return """fof(conj1, conjecture, s__orientation(s__{obj1},s__{obj2},s__{prep})).
         #        """.format(obj1=obj1.title(),
         #                   obj2=obj2.title(),
         #                   prep=prep)
         prep = preposition.title().replace("_", "")
-        return "fof(conj1, conjecture, ((? [V__X, V__Y]: s__orientation(V__X, V__Y, s__{})) )).".format(prep.title())
-
-
+        # return "fof(conj1, conjecture, ((? [V__X, V__Y]: s__orientation(V__X, V__Y, s__{})) )).".format(prep.title())
+        return "fof(conj1,conjecture, ( s__orientation(s__{obj1}__m,s__{obj2}__m,s__{prep}) )).".format(obj1=obj1_,
+                                                                                                        obj2=obj2_,
+                                                                                                        prep=prep)
 
     def prover(self, image, query):
         try:
@@ -86,13 +88,14 @@ class KnowledgeBase:
         objects = set(self.df[self.df.image == image].object1) & set(self.df[self.df.image == image].object2)
         if noun1 not in objects or noun2 not in objects:
             return image, None, []
+        index = self.images.index(image)
 
-        tptp_query = self.tptp_query(noun1, noun2, preposition)
         irrc_fp = tempfile.NamedTemporaryFile(delete=False)
-        for axiom in self.tptp_by_image(image):
+        for axiom in self.tptp_by_image(image, index):
             irrc_fp.write(axiom + "\n")
         irrc_fp.close()
 
+        tptp_query = self.tptp_query(noun1, noun2, preposition, index)
         problems_fp = tempfile.NamedTemporaryFile(delete=False)
         problems_fp.write(tptp_query)
         problems_fp.close()
@@ -116,6 +119,6 @@ class KnowledgeBase:
         return image, proved, response
 
     def runquery(self, query):
-        for image in self.images[:50]:
+        for image in self.images:
             image, imname, _ = self.prover(image=image, query=query)
             yield image, imname
